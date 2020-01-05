540f89e9be08e1b2dd32d44eb2339cbb9c05eabd
Logan Mc <logangmcnulty@gmail.com>
2020-01-05 15:04:02 -0500

    Updates

---
+++ precache-manifest.7b8c41cd03bdbaca940bf9a594242e25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ea067905c48977b0705fe3d90e38e88",
+    "url": "/React-Click-Game/index.html"
+  },
+  {
+    "revision": "b9c94d04e922b3805e4b",
+    "url": "/React-Click-Game/static/css/main.030075c1.chunk.css"
+  },
+  {
+    "revision": "fba3e220fb0ffa09b834",
+    "url": "/React-Click-Game/static/js/2.70145859.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React-Click-Game/static/js/2.70145859.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b9c94d04e922b3805e4b",
+    "url": "/React-Click-Game/static/js/main.ad6fb91f.chunk.js"
+  },
+  {
+    "revision": "f598c0d5a3594bde8aaf",
+    "url": "/React-Click-Game/static/js/runtime-main.a3b652ac.js"
+  }
+]);
